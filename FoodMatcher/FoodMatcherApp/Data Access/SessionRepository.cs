ee6ebfc95d4e924c79641405c6fd9934e14d45b0
Chris LoJacono <clojacono92@gmail.com>
2021-05-25 11:51:57 -0500

    session repo

---
+++ FoodMatcher/FoodMatcherApp/Data Access/SessionRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Dapper;
+using FoodMatcherApp.Models;
+
+namespace FoodMatcherApp.Data_Access
+{
+    public class SessionRepository
+    {
+        const string ConnectionString = "Server=localhost;Database=FoodMatcher;Trusted_Connection=True;";
+
+        public List<Session> GetAllSessions()
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = "Select * From Sessions";
+
+            return db.Query<Session>(sql).ToList();
+        }
+
+        public void AddASession(Session session)
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"INSERT INTO [dbo].[Sessions]([Location],[Category],[SearchTerm],[CreatedDate])
+                        OUTPUT inserted.Id
+                        VALUES(@Location,@Category,@SearchTerm,@CreatedDate)";
+
+            var id = db.ExecuteScalar<Guid>(sql, session);
+
+            session.Id = id;
+
+
+        }
+    }
+}
