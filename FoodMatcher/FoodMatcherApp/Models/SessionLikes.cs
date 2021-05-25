cb41fa6a6398ea71aaaf4b4bc25cf377c4bd61b1
Chris LoJacono <clojacono92@gmail.com>
2021-05-25 10:01:02 -0500

    session likes

---
+++ FoodMatcher/FoodMatcherApp/Models/SessionLikes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodMatcherApp.Models
+{
+    public class SessionLikes
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string RestaurantId { get; set; }
+        public Guid SessionId { get; set; }
+
+    }
+}
