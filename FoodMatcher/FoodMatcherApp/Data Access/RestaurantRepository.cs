13bbbe45e115e09451239bfd3ce4f01db8267ec5
Chris LoJacono <clojacono92@gmail.com>
2021-05-25 10:39:46 -0500

    save

---
+++ FoodMatcher/FoodMatcherApp/Data Access/RestaurantRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Dapper;
+using FoodMatcherApp.Models;
+
+namespace FoodMatcherApp.Data_Access
+{
+    public class RestaurantRepository
+    {
+        const string ConnectionString = "Server=localhost;Database=FoodMatcher;Trusted_Connection=True;";
+
+        public List<Restaurant> GetAllRestaurants()
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = "Select * From Restaurants";
+
+            return db.Query<Restaurant>(sql).ToList();
+        }
+
+    }
+}
