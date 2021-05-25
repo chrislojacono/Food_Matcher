afe7a2764f0104fd074a40d3a28821575910e2ac
Chris LoJacono <clojacono92@gmail.com>
2021-05-25 10:34:50 -0500

    user repository

---
+++ FoodMatcher/FoodMatcherApp/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodMatcherApp.Controllers
+{
+    [Route("api/Users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
---
+++ FoodMatcher/FoodMatcherApp/Data Access/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Dapper;
+using FoodMatcherApp.Models;
+
+namespace FoodMatcherApp.Data_Access
+{
+    public class UserRepository
+    {
+        const string ConnectionString = "Server=localhost;Database=FoodMatcher;Trusted_Connection=True;";
+
+        public List<User> GetAllUsers()
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = "Select * From Users";
+
+            return db.Query<User>(sql).ToList();
+        }
+    }
+}
