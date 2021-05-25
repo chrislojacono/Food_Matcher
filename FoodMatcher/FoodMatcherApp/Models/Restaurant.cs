c8d6db3fe20fab26fe1079468720293db4655316
Chris LoJacono <clojacono92@gmail.com>
2021-05-25 09:57:42 -0500

    Rstaurant Model

---
+++ FoodMatcher/FoodMatcherApp/Models/Restaurant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodMatcherApp.Models
+{
+    public class Restaurant
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Rating { get; set; }
+        public string Image_Url { get; set; }
+        public string Url { get; set; }
+        public Int64 Distance { get; set; }
+
+    }
+}
