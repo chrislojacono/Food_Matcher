587f7cca7050d81851c831027dc9bacb4cf23ef0
Chris LoJacono <clojacono92@gmail.com>
2021-05-26 19:10:18 -0500

    userData

---
+++ foodMatcher.ui/src/Helpers/Data/UserData.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { baseUrl } from './config.json';
+
+const userDataUrl = `${baseUrl}/Users`;
+
+const AddAUser = (userObject) => new Promise((resolve, reject) => {
+  axios.post(userDataUrl, userObject).then((response) => {
+    resolve(response.data);
+  }).catch((error) => reject(error));
+});
+
+export default { AddAUser };
