4ae4ecf84be779a0cf1bd4861a84ec250ad677a2
Chris LoJacono <clojacono92@gmail.com>
2021-05-30 15:27:23 -0500

    cool cards saved for today

--- foodMatcher.ui/src/Components/Cards/MatchCard.js
+++ foodMatcher.ui/src/Components/Cards/MatchCard.js
@@ -5,9 +5,12 @@ import {
   Heading,
   Badge,
   Text,
+  Button,
+  Flex,
 } from '@chakra-ui/react';
+import { StarIcon } from '@chakra-ui/icons';
 
-export default function CarouselCard({ yelpData }) {
+export default function MatchCard({ yelpData }) {
   return (
     <Box w='350px' rounded='20px' overflow='hidden' boxShadow='md' bg='gray.200'>
       <Image src={yelpData.image_Url} alt='carousel' objectFit='contain' />
@@ -17,10 +20,17 @@ export default function CarouselCard({ yelpData }) {
         <Heading fontSize='xl' fontWeight='semibold'>
           {yelpData.name}
         </Heading>
-        <Text textTransform='uppercase' fontSize='md' color='gray.700'>
-          {yelpData.rating}/5 stars &bull; {yelpData.distance} meters away
+        <Text textTransform='initial' fontSize='md' m={2} color='gray.700'>
+          {Array(yelpData.rating).fill('').map((_, i) => (
+            <StarIcon color='yellow.400'/>
+          ))} &bull; {parseFloat(yelpData.distance * 0.00062137).toFixed(2)} mile(s) away
         </Text>
       </Box>
+      <Flex justify='center' alignItems='center'>
+      <a href={yelpData.yelpUrl} target='_blank' rel='noreferrer' className='anchors'>
+      <Button bg='teal.300' ml='auto' mb={2}>More Details</Button>
+      </a>
+      </Flex>
     </Box>
   );
 }
