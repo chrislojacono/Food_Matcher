2b4c7cdbd8daac8f93bf956ead2da16234fcbf83
Chris LoJacono <clojacono92@gmail.com>
2021-06-04 12:21:43 -0500

    gradient onhome view

--- letsEat.ui/src/Components/Views/HomeView.js
+++ letsEat.ui/src/Components/Views/HomeView.js
@@ -9,7 +9,7 @@ import {
 export default class HomePageView extends Component {
   render() {
     return (
-      <Flex height="40%" width="40%" alignItems="center" background="whitesmoke" mt="3%" mb="10%" justifyContent="center" direction='column' p="40" rounded={6}>
+      <Flex height="40%" width="40%" alignItems="center" background="whitesmoke" mt="3%" mb="10%" justifyContent="center" direction='column' p="40" rounded={6} bgGradient="linear(red.100 25%, orange.100 50%, yellow.100 100%)">
         <Heading mb={6} whiteSpace="nowrap">Welcome to Let's Eat!</Heading>
         <Link to={{
           pathname: '/SessionForm',
