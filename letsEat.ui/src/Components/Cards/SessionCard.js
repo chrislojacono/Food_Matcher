f7828ef8562ede7cb23c100cb634a4481d26b67b
Chris LoJacono <clojacono92@gmail.com>
2021-06-04 12:36:24 -0500

    name of other user on session cards'

--- letsEat.ui/src/Components/Cards/SessionCard.js
+++ letsEat.ui/src/Components/Cards/SessionCard.js
@@ -6,10 +6,34 @@ import {
   Button,
 } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
+import UserData from '../../Helpers/Data/UserData';
 
 export default class SessionCard extends Component {
+  state = {
+    otherUser: '',
+    sessionData: this.props.sessionData,
+    myUserId: this.props.userId,
+  }
+
+  componentDidMount() {
+    const { sessionData, myUserId } = this.state;
+    if (sessionData.user1Id === myUserId) {
+      UserData.GetSingleUser(sessionData.user2Id).then((response) => {
+        this.setState({
+          otherUser: response,
+        });
+      });
+    } else {
+      UserData.GetSingleUser(sessionData.user1Id).then((response) => {
+        this.setState({
+          otherUser: response,
+        });
+      });
+    }
+  }
+
   render() {
-    const { sessionData } = this.props;
+    const { sessionData, otherUser } = this.state;
     const date = new Date(sessionData.createdDate);
     return (
       <Flex
@@ -19,11 +43,14 @@ export default class SessionCard extends Component {
         width='auto'
         border='3px'
         borderColor='turquoise'
-        boxShadow='md'
+        boxShadow='lg'
         margin={5}
+        background='whitesmoke'
+        rounded={6}
       >
-        <Heading whiteSpace="nowrap" p={5} fontSize="1.1rem">Session from {date.toDateString()}</Heading>
+        <Heading whiteSpace="nowrap" p={5} fontSize="1.1rem" letterSpacing='wide'>Session with {otherUser.firstName}</Heading>
         <Text fontSize='lg' p={5}>Looking for {sessionData.searchTerm} Food</Text>
+        <Text fontSize='medium' color='gray.400'>{date.toDateString()}</Text>
         <Flex>
         <Link to={{
           pathname: `/session/${sessionData.id}`,
