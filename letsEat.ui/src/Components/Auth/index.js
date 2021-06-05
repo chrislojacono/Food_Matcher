71d2481f42bba1217c5721f3d7c00058490ee7f1
Chris LoJacono <clojacono92@gmail.com>
2021-06-04 21:26:08 -0500

    format auth

--- letsEat.ui/src/Components/Auth/index.js
+++ letsEat.ui/src/Components/Auth/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import 'firebase/auth';
-import {
-  Flex,
-} from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 import googleImage from './google.png';
 import AuthData from '../../Helpers/Data/AuthData';
 
 class Auth extends React.Component {
   render() {
     return (
-      <Flex className='Auth' marginTop="20%">
-          <button onClick={AuthData.loginClickEvent} className='btn btn-secondary login'>
-          <img className='loginButtonImg'src={googleImage} alt='google sign in' />
+      <Flex className='Auth' marginTop='20%'>
+        <button
+          onClick={AuthData.loginClickEvent}
+          className='btn btn-secondary login'
+        >
+          <img
+            className='loginButtonImg'
+            src={googleImage}
+            alt='google sign in'
+          />
         </button>
       </Flex>
     );
