305430086a7788d78935af0c23c720b6210c494e
Chris LoJacono <clojacono92@gmail.com>
2021-06-06 10:35:57 -0500

    gradient change

--- letsEat.ui/src/Components/Views/HomeView.js
+++ letsEat.ui/src/Components/Views/HomeView.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import {
-  Box,
-  Button,
-  Flex,
-  Heading,
-  Image,
-} from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, Image } from '@chakra-ui/react';
 import mainGif from '../../Helpers/Images/LetsEat.gif';
 
 export default class HomePageView extends Component {
@@ -23,20 +17,26 @@ export default class HomePageView extends Component {
         direction='column'
         p='10'
         rounded={6}
-        bgGradient='linear(blue.100 25%, green.100 50%, orange.100 100%)'
+        bgGradient='linear(green.200 25%, orange.100 50%, green.200 100%)'
       >
-        <Heading mb={6} whiteSpace='nowrap' textShadow='xl'>
+        {/* <Heading mb={6} whiteSpace='nowrap' textShadow='xl'>
           Welcome to Let's Eat!
-        </Heading>
+        </Heading> */}
         <Box size='lg'>
-          <Image src={mainGif} alt='gif' objectFit='cover'/>
+          <Image
+            src={mainGif}
+            alt='gif'
+            objectFit='cover'
+            rounded={20}
+            boxShadow='xl'
+          />
         </Box>
         <Link
           to={{
             pathname: '/SessionForm',
           }}
         >
-          <Button colorScheme='teal' size='lg' mt='10'>
+          <Button colorScheme='telegram' size='lg' mt='10'>
             Start a Session!
           </Button>
         </Link>
