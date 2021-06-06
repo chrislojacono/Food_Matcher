4eb343cdd4b175c8852dbad5ef6cb115cd324e13
Chris LoJacono <clojacono92@gmail.com>
2021-06-06 10:27:13 -0500

    logo

--- letsEat.ui/src/Components/Views/HomeView.js
+++ letsEat.ui/src/Components/Views/HomeView.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Flex, Heading } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Image,
+} from '@chakra-ui/react';
+import mainGif from '../../Helpers/Images/LetsEat.gif';
 
 export default class HomePageView extends Component {
   render() {
@@ -14,13 +21,16 @@ export default class HomePageView extends Component {
         mb='10%'
         justifyContent='center'
         direction='column'
-        p='40'
+        p='10'
         rounded={6}
         bgGradient='linear(blue.100 25%, green.100 50%, orange.100 100%)'
       >
         <Heading mb={6} whiteSpace='nowrap' textShadow='xl'>
           Welcome to Let's Eat!
         </Heading>
+        <Box size='lg'>
+          <Image src={mainGif} alt='gif' objectFit='cover'/>
+        </Box>
         <Link
           to={{
             pathname: '/SessionForm',
