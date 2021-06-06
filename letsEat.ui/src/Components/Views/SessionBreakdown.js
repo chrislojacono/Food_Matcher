d92e08b1bb9ed69730df9e17f7ede5fdd902ba70
Chris LoJacono <clojacono92@gmail.com>
2021-06-06 12:35:17 -0500

    session breakdown in hooks

--- letsEat.ui/src/Components/Views/SessionBreakdown.js
+++ letsEat.ui/src/Components/Views/SessionBreakdown.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Flex, Heading, Button } from '@chakra-ui/react';
 import SessionLikesData from '../../Helpers/Data/SessionLikeData';
 import NonMatchCard from '../Cards/NonMatchCard';
@@ -8,193 +8,190 @@ import FinalCard from '../Cards/FinalCard';
 import RestaurantData from '../../Helpers/Data/RestaurantData';
 import SessionData from '../../Helpers/Data/SessionData';
 
-export default class SessionMatchesView extends Component {
-  state = {
-    yourLikedRestaurants: [],
-    matches: [],
-    userId: this.props.user?.id,
-    sessionId: this.props.match.params.id,
-    finalDecision: '',
-    sessionObject: '',
+export default function SessionMatchesView(props) {
+  const [yourLikedRestaurants, setYourLikedRestaurants] = useState([]);
+  const [matches, setMatches] = useState([]);
+  const [sessionId] = useState(props.match.params.id);
+  const [userId] = useState(props.user?.id);
+  const [finalDecision, setFinalDecision] = useState('');
+  const [sessionObject, setSessionObject] = useState('');
+
+  useEffect(() => {
+    loadContent();
+    getFinalDecision();
+    getSessionData();
+  });
+
+  const loadContent = () => {
+    SessionLikesData.GetMatches(props.match.params.id).then((response) => {
+      setMatches(response);
+    });
+    SessionLikesData.GetLikesOfAUserPerSession(
+      props.user?.id,
+      props.match.params.id,
+    ).then((response) => {
+      setYourLikedRestaurants(response);
+    });
   };
 
-  componentDidMount() {
-    this.loadContent();
-    this.getFinalDecision();
-    this.getSessionData();
-  }
-
-  loadContent = () => {
-    const { userId, sessionId } = this.state;
-    SessionLikesData.GetMatches(sessionId).then((response) => {
-      this.setState({
-        matches: response,
-      });
+  const getSessionData = () => {
+    SessionData.GetASession(sessionId).then((response) => {
+      setSessionObject(response);
     });
-    SessionLikesData.GetLikesOfAUserPerSession(userId, sessionId).then(
-      (response) => {
-        this.setState({
-          yourLikedRestaurants: response,
-        });
-      },
-    );
-  }
+  };
 
-  getSessionData = () => {
-    SessionData.GetASession(this.state.sessionId).then((response) => {
-      this.setState({
-        sessionObject: response,
-      });
-    });
-  }
-
-  getFinalDecision = () => {
-    const { sessionId } = this.state;
+  const getFinalDecision = () => {
     FinalDecisionData.GetAFinalDecision(sessionId).then((response) => {
-      this.setState({
-        finalDecision: response,
-      });
+      setFinalDecision(response);
     });
   };
 
-  makeAFinalDecision = (restaurantId) => {
-    const { sessionId } = this.state;
+  const makeAFinalDecision = (restaurantId) => {
     const finalObject = {
       SessionId: sessionId,
       RestaurantId: restaurantId,
     };
     FinalDecisionData.AddAFinalDecision(finalObject).then(() => {
-      this.getFinalDecision();
+      getFinalDecision();
     });
   };
 
-  removeALike = (restaurantId) => {
-    const { sessionId, userId } = this.state;
+  const removeALike = (restaurantId) => {
     SessionLikesData.RemoveALike(userId, sessionId, restaurantId).then(() => {
-      this.loadContent();
+      loadContent();
     });
-  }
+  };
 
-  getRandomRestaurant = () => {
-    const { sessionId } = this.state;
+  const getRandomRestaurant = () => {
     RestaurantData.GetRandomRestaurant(sessionId).then((response) => {
-      this.makeAFinalDecision(response.id);
+      makeAFinalDecision(response.id);
     });
-  }
+  };
 
-  render() {
-    const {
-      matches,
-      yourLikedRestaurants,
-      finalDecision,
-      sessionObject,
-    } = this.state;
-    return (
+  return (
+    <Flex
+      height='70%'
+      width='70%'
+      alignItems='center'
+      background='grey.200'
+      mt='1%'
+      mb='10%'
+      justifyContent='center'
+      direction='column'
+      rounded={6}
+    >
       <Flex
-        height='70%'
-        width='70%'
-        alignItems='center'
-        background='grey.200'
-        mt='1%'
-        mb='10%'
         justifyContent='center'
         direction='column'
-        rounded={6}
+        alignItems='center'
+        flexWrap='wrap'
       >
-        <Flex
-          justifyContent='center'
-          direction='column'
-          alignItems='center'
-          flexWrap='wrap'
-        >
-          {finalDecision !== '' && (
-            <Flex
-              direction='column'
-              justify='center'
-              align='center'
-              bg='green.200'
-              w='98%'
-              rounded='20px'
-              my={2}
-            >
-                <>
-                  <Heading m={2} p={4} textDecoration='underline' rounded={4} whiteSpace='nowrap'>
-                    The Final!
-                  </Heading>
-                  <Flex justify='center' align='center' flexWrap='wrap'>
-                    <FinalCard
-                      key={finalDecision.id}
-                      yelpData={finalDecision}
-                    />
-                  </Flex>
-                </>
-            </Flex>
-          )}
-          {sessionObject.user2Id !== null && (
-          <Button colorScheme='orange' onClick={this.getRandomRestaurant}>Decide For Us!</Button>
-          )}
-          {matches.length ? (
-          <Flex
-          direction='column'
-          justify='center'
-          align='center'
-          bg='blue.200'
-          w='98%'
-          rounded='20px'
-          my={2}
-        >
-          <Heading m={2} p={4} textDecoration='underline' rounded={4} whiteSpace='nowrap'>
-            You guys agreed on
-          </Heading>
-          <Flex
-            justify='center'
-            align='center'
-            flexWrap='wrap'
-            bg='blue.200'
-            w='98%'
-            rounded='20px'
-          >
-            {matches.map((restaurant) => (
-              <MatchCard
-                key={restaurant.id}
-                yelpData={restaurant}
-                makeFinalDecision={this.makeAFinalDecision}
-              />
-            ))}
-          </Flex>
-        </Flex>
-          ) : (<></>)}
+        {finalDecision !== '' && (
           <Flex
             direction='column'
             justify='center'
             align='center'
-            bg='blanchedAlmond'
+            bg='green.200'
             w='98%'
             rounded='20px'
             my={2}
           >
-            <Heading m={2} p={4} textDecoration='underline' rounded={4} whiteSpace='nowrap'>
-              Your Likes
+            <>
+              <Heading
+                m={2}
+                p={4}
+                textDecoration='underline'
+                rounded={4}
+                whiteSpace='nowrap'
+              >
+                The Final!
+              </Heading>
+              <Flex justify='center' align='center' flexWrap='wrap'>
+                <FinalCard key={finalDecision.id} yelpData={finalDecision} />
+              </Flex>
+            </>
+          </Flex>
+        )}
+        {sessionObject.user2Id !== null && (
+          <Button colorScheme='orange' onClick={getRandomRestaurant}>
+            Decide For Us!
+          </Button>
+        )}
+        {matches.length ? (
+          <Flex
+            direction='column'
+            justify='center'
+            align='center'
+            bg='blue.200'
+            w='98%'
+            rounded='20px'
+            my={2}
+          >
+            <Heading
+              m={2}
+              p={4}
+              textDecoration='underline'
+              rounded={4}
+              whiteSpace='nowrap'
+            >
+              You guys agreed on
             </Heading>
             <Flex
               justify='center'
-              alignItems='center'
+              align='center'
               flexWrap='wrap'
-              bg='blanchedalmond'
+              bg='blue.200'
               w='98%'
               rounded='20px'
             >
-              {yourLikedRestaurants.map((restaurant) => (
-                <NonMatchCard
+              {matches.map((restaurant) => (
+                <MatchCard
                   key={restaurant.id}
                   yelpData={restaurant}
-                  removeALike={this.removeALike}
+                  makeFinalDecision={makeAFinalDecision}
                 />
               ))}
             </Flex>
           </Flex>
+        ) : (
+          <></>
+        )}
+        <Flex
+          direction='column'
+          justify='center'
+          align='center'
+          bg='blanchedAlmond'
+          w='98%'
+          rounded='20px'
+          my={2}
+        >
+          <Heading
+            m={2}
+            p={4}
+            textDecoration='underline'
+            rounded={4}
+            whiteSpace='nowrap'
+          >
+            Your Likes
+          </Heading>
+          <Flex
+            justify='center'
+            alignItems='center'
+            flexWrap='wrap'
+            bg='blanchedalmond'
+            w='98%'
+            rounded='20px'
+          >
+            {yourLikedRestaurants.map((restaurant) => (
+              <NonMatchCard
+                key={restaurant.id}
+                yelpData={restaurant}
+                removeALike={removeALike}
+              />
+            ))}
+          </Flex>
         </Flex>
       </Flex>
-    );
-  }
+    </Flex>
+  );
 }
