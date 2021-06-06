1caf24aaaa1ec03b93e979f984a480fb887c200c
Chris LoJacono <clojacono92@gmail.com>
2021-06-06 10:01:29 -0500

    limit raised to 50

--- letsEat.ui/functions/get-yelp/get-yelp.js
+++ letsEat.ui/functions/get-yelp/get-yelp.js
@@ -5,11 +5,12 @@ exports.handler = function (event, context, callback) {
   const { REACT_APP_YELP_API_KEY } = process.env;
   const client = yelp.client(REACT_APP_YELP_API_KEY);
 
-  const { term, location } = event.queryStringParameters;
+  const { term, location, limit } = event.queryStringParameters;
 
   const searchRequest = {
     term,
     location,
+    limit
   };
   console.warn(event.queryStringParameters);
   const fetchFromYelp = async function () {
